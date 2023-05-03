75049b2ddbcb9422b7286da31203ec266a94f7cf
Max Mezalon <jmezalon@gmail.com>
2023-05-03 19:53:35 -0400

    enter a console log

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
-import Constants from 'expo-constants';
-import { colors } from './src/utils/colors';
-import { Focus } from './src/features/Focus';
-import { Timer } from './src/features/Timer';
+import React, { useState } from "react";
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from "react-native";
+import Constants from "expo-constants";
+import { colors } from "./src/utils/colors";
+import { Focus } from "./src/features/Focus";
+import { Timer } from "./src/features/Timer";
 
 export default function App() {
+  console.log("there is an error in timer component");
+
   const [currentSubject, setCurrentSubject] = useState(null);
   const [previousSubjects, setPreviousSubject] = useState([]);
 
@@ -38,7 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     backgroundColor: colors.beige,
   },
 });
